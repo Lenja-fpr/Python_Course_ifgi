f5baa75c9d141873b2a5d4ae9e1a4db8d5a05529
Lenja <lenja@fipper.de>
2026-04-28 13:10:06 +0200

    added solution for ex. 3 (remove_adjacent())

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -30,29 +30,49 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
-# def remove_adjacent(nums):
-# +++your code here+++
-# return
+def remove_adjacent(nums):
+    
+    # create a list for the reduced version of nums
+    reducedNums = []
+
+    # for every element in nums
+    for i in nums:
+
+        # set isInReducedNums to False
+        isInReducedNums = False
+
+        # for every element in reducedNums:
+        for j in reducedNums:
+            # if the element from nums already in the reducedNums list, set isInReducedNums to True
+            if i == j:
+                isInReducedNums = True
+
+        # if the element is not in the reducedNums list, add it to the list
+        if isInReducedNums == False:
+            reducedNums.append(i)
+
+    # return the reduced version of the list
+    return reducedNums
 
 
 
-#def main():
-#print('donuts')
-#print(donuts(4))
-#print(donuts(9))
-#print(donuts(10))
-#print(donuts('twentyone'))
+def main():
+    #print('donuts')
+    #print(donuts(4))
+    #print(donuts(9))
+    #print(donuts(10))
+    #print(donuts('twentyone'))
 
-#print('verbing')
-#print(verbing('hail'))
-#print(verbing('swiming'))
-#print(verbing('do'))
+    #print('verbing')
+    #print(verbing('hail'))
+    #print(verbing('swiming'))
+    #print(verbing('do'))
 
-#print('remove_adjacent')
-#print(remove_adjacent([1, 2, 2, 3]))
-#print(remove_adjacent([2, 2, 3, 3, 3]))
-#print(remove_adjacent([]))
+    print('remove_adjacent')
+    print(remove_adjacent([1, 2, 2, 3]))
+    print(remove_adjacent([2, 2, 3, 3, 3]))
+    print(remove_adjacent([]))
 
 # Standard boilerplate to call the main() function.
-#if __name__ == '__main__':
-#main()
+if __name__ == '__main__':
+    main()
