5fbde6b55c4cedae783ab339fb847247b7943ed4
Lenja <lenja@fipper.de>
2026-04-28 12:40:08 +0200

    upload tasks pdf

--- exercise_2/exercise_2.py
+++ exercise_2/exercise_2.py
@@ -1,36 +1,39 @@
 # donuts
 
-# def donuts(count):
-# +++your code here+++
 # Given an integer count of a number of donuts, return a string
 # of the form 'Number of donuts: <count>', where <count> is the number
 # passed in. However, if the count is 10 or more, then use the word 'many'
 # instead of the actual count.
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
-# return
+#def donuts(count):
+# +++ your code here +++
+#return
 
 
 # verbing
-# def verbing(s):
-# +++your code here+++
+
 # Given a string, if its length is at least 3,
 # add 'ing' to its end.
 # Unless it already ends in 'ing', in which case
 # add 'ly' instead.
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
+# def verbing(s):
+# +++your code here+++
 # return
 
 
 # Remove adjacent
-# def remove_adjacent(nums):
-# +++your code here+++
+
 # Given a list of numbers, return a list where
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
+# def remove_adjacent(nums):
+# +++your code here+++
 # return
+
 
 
 #def main():
