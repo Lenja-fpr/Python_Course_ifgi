8a5eb4d50ceea68d36967483f2fb896194ef065c
Lenja <lenja@fipper.de>
2026-08-12 14:05:14 +0200

    added code for comparing the place to the floodrisk map of NRW

--- final_project/FloodRiskAnalysis.py
+++ final_project/FloodRiskAnalysis.py
@@ -1,13 +1,71 @@
-# Calculate the distance from a given point to the next water body, return the distance and the name of the water
-#imports
+### --- imports ---
 import arcpy
 import requests
 
-# input
+
+### --- input ---
 selected_place = arcpy.GetParameterAsText(0)
 
-# analysis
-# get nearest water
+
+### --- Compare the selected place to the Floodrisk Map of NRW ---
+
+# is the point inside an area with a high risk?
+# check by using select by location
+arcpy.management.SelectLayerByLocation(
+    in_layer="ueberflutungsgrenzen_hohe_Wahrscheinlichkeit",
+    overlap_type="INTERSECT",
+    select_features=selected_place,
+    selection_type="NEW_SELECTION",
+    invert_spatial_relationship="NOT_INVERT"
+)
+# check if a feature is selected
+if int(arcpy.management.GetCount("ueberflutungsgrenzen_hohe_Wahrscheinlichkeit")[0]) != 0:
+    arcpy.AddMessage("The given place is located in an area with a high risk of flooding")
+
+    # clear the selection
+    arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_hohe_Wahrscheinlichkeit","CLEAR_SELECTION")
+
+else:
+    # is the point in an area with a middle risk?
+    # check by using select by location
+    arcpy.management.SelectLayerByLocation(
+        in_layer="ueberflutungsgrenzen_mittlere_Wahrscheinlichkeit",
+        overlap_type="INTERSECT",
+        select_features=selected_place,
+        selection_type="NEW_SELECTION",
+        invert_spatial_relationship="NOT_INVERT"
+    )
+    #check if a feature is selected
+    if int(arcpy.management.GetCount("ueberflutungsgrenzen_mittlere_Wahrscheinlichkeit")[0]) != 0:
+        arcpy.AddMessage("The given place is located in an area with a moderate risk of flooding")
+    
+        #clear the selection
+        arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_mittlere_Wahrscheinlichkeit","CLEAR_SELECTION")
+
+    else:
+        # is the point in an area with a low risk?
+        #check by using select by location
+        arcpy.management.SelectLayerByLocation(
+            in_layer="ueberflutungsgrenzen_niedrige Wahrscheinlichkeit",
+            overlap_type="INTERSECT",
+            select_features=selected_place,
+            selection_type="NEW_SELECTION",
+            invert_spatial_relationship="NOT_INVERT"
+        )
+        #check if a feature is selected
+        if int(arcpy.management.GetCount("ueberflutungsgrenzen_niedrige Wahrscheinlichkeit")[0]) != 0:
+            arcpy.AddMessage("The given place is located in an area with a low risk of flooding")
+            
+            #clear the selection
+            arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_niedrige Wahrscheinlichkeit","CLEAR_SELECTION")
+
+        else:
+            # the point is in an area without a risk
+            arcpy.AddMessage("The given place is located in an area without a risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')")
+
+
+### --- find the nearest river ---
+# get nearest river
 arcpy.analysis.Near(
     in_features= selected_place,
     near_features="gsk3e_gewkz_line_breite",
@@ -19,11 +77,11 @@ arcpy.analysis.Near(
     distance_unit="Meters"
 )
 
-# get datails of the nearest water
+# get datails of the nearest river
 with arcpy.da.SearchCursor(selected_place, ["NEAR_DIST", "NEAR_FID"]) as cur:
     for row in cur:
-        distance = row[0] # distance to the closest water
-        near_fid = row[1] # ObjectID of the closest water
+        distance = row[0] # distance to the closest river
+        near_fid = row[1] # ObjectID of the closest river
         
 where = f"FID = {near_fid}"
 
@@ -33,11 +91,12 @@ with arcpy.da.SearchCursor("gsk3e_gewkz_line_breite", ["FID", "GEWHNAME", "ST_BR
         nearestWidth = row[2]
 
 # output
-arcpy.AddMessage(f"Nearest water: {nearestName}")
-arcpy.AddMessage(f"Distance to the nearest Water: {distance}")
-arcpy.AddMessage(f"Witdh of the nearest Water: {nearestWidth}m")
+arcpy.AddMessage(f"Nearest river: {nearestName}")
+arcpy.AddMessage(f"Distance to the nearest river: {distance}")
+arcpy.AddMessage(f"Witdh of the nearest river: {nearestWidth}m")
 
-# Get water level data from the nearest river via an API
+
+### --- Get water level data from the nearest river via an API ---
 #river = nearestName.upper()
 river = "RHEIN" # delete this later when the API stuff below is fully implemented
 
