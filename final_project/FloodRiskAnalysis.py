66ef6f0512eb27e384c26046197a8c498c3d5b89
Kian Jay Lenert <klenert@uni-muenster.de>
2026-08-17 14:12:55 +0200

    merge from dev-kjl

--- final_project/FloodRiskAnalysis.py
+++ final_project/FloodRiskAnalysis.py
@@ -5,6 +5,12 @@ import openmeteo_requests
 import pandas as pd
 import requests_cache
 from retry_requests import retry
+import os
+from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+
 
 ### --- input ---
 selected_place = arcpy.GetParameterAsText(0)
@@ -23,7 +29,7 @@ arcpy.management.SelectLayerByLocation(
 )
 # check if a feature is selected
 if int(arcpy.management.GetCount("ueberflutungsgrenzen_hohe_wahrscheinlichkeit")[0]) != 0:
-    arcpy.AddMessage("The given place is located in an area with a high risk of flooding")
+    risk_text = "The given place is located in an area with a high risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')."
 
     # clear the selection
     arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_hohe_wahrscheinlichkeit","CLEAR_SELECTION")
@@ -40,8 +46,8 @@ else:
     )
     #check if a feature is selected
     if int(arcpy.management.GetCount("ueberflutungsgrenzen_mittlere_wahrscheinlichkeit")[0]) != 0:
-        arcpy.AddMessage("The given place is located in an area with a moderate risk of flooding")
-    
+        risk_text = "The given place is located in an area with a moderate risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')."
+
         #clear the selection
         arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_mittlere_wahrscheinlichkeit","CLEAR_SELECTION")
 
@@ -57,14 +63,15 @@ else:
         )
         #check if a feature is selected
         if int(arcpy.management.GetCount("ueberflutungsgrenzen_niedrige Wahrscheinlichkeit")[0]) != 0:
-            arcpy.AddMessage("The given place is located in an area with a low risk of flooding")
-            
+            risk_text = "The given place is located in an area with a low risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')."
+
             #clear the selection
             arcpy.SelectLayerByAttribute_management("ueberflutungsgrenzen_niedrige Wahrscheinlichkeit","CLEAR_SELECTION")
 
         else:
             # the point is in an area without a risk
-            arcpy.AddMessage("The given place is located in an area without a risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')")
+            risk_text = f"The given place is located in an area without a risk of flooding (compared to the 'Hochwasser-Gefahrenkarte NRW')."
+        arcpy.AddMessage(risk_text)
 
 
 ### --- find the nearest river ---
@@ -102,14 +109,109 @@ with arcpy.da.SearchCursor("gsk3e_gewkz_line_breite", ["FID", "GEWHNAME", "ST_BR
         nearestWidth = row[2]
 
 # output
-arcpy.AddMessage(f"\nNearest river: {nearestName}")
-arcpy.AddMessage(f"Distance to the nearest river: {distance} m")
-arcpy.AddMessage(f"Witdh of the nearest river: {nearestWidth} m")
+arcpy.AddMessage(f"Nearest river: {nearestName}")
+arcpy.AddMessage(f"Distance to the nearest river: {distance}m")
+arcpy.AddMessage(f"Width of the nearest river: {nearestWidth}m")
 
 
-### --- Get water level data from the nearest river via an API ---
-#river = nearestName.upper()
-river = "RHEIN" # delete this later when the API stuff below is fully implemented
+# -------- Get water level data from the nearest river via an API ---------------
+
+river = nearestName.upper()
+
+# handle Umlaute in the river name for the API request
+if "Ä" in river:
+    river = river.replace("Ä", "%C3%84")
+if "Ö" in river:
+    river = river.replace("Ö", "%C3%96")
+if "Ü" in river:
+    river = river.replace("Ü", "%C3%9C")
+
+# hande spaces:
+if " " in river:
+    river = river.replace(" ", "%20")
+
+
+# function to find the nearest measuring station to a given point from a json list of stations 
+def findNearestStation(json):
+    # collect the uuids, names and coordinates of all stations in a list
+    stations_list = []
+    for station in json:
+        if 'longitude' in station:
+            uuid = station['uuid']
+            name = station['longname']
+            lon = station['longitude']
+            lat = station['latitude']
+            stations_list.append([uuid, name, (lon, lat)])
+        
+    # add the measuring stations to a feature class and add it to the map:
+    # get the currently active geodatabase
+    aprx = arcpy.mp.ArcGISProject("CURRENT")
+    map_obj = aprx.activeMap
+    gdb = aprx.defaultGeodatabase
+    fc_path = os.path.join(gdb, "measuring_stations")
+    # create a new featureclass (overwrite if it already exists)
+    for layer in map_obj.listLayers():
+        if layer.name == "measuring_stations":
+            if layer.isFeatureLayer:
+                layer_path = layer.dataSource
+            if os.path.normcase(layer_path) == os.path.normcase(fc_path):
+                map_obj.removeLayer(layer)
+                #del layer
+    if arcpy.Exists(fc_path):
+        arcpy.management.Delete(fc_path)
+    arcpy.management.CreateFeatureclass(gdb, "measuring_stations", "POINT", spatial_reference=arcpy.SpatialReference(4326))
+    # add fields to the featureclass
+    arcpy.management.AddField("measuring_stations", "uuid", "TEXT")
+    arcpy.management.AddField("measuring_stations", "name", "TEXT")
+    
+    # fill the featureclass with the data from the table
+    with arcpy.da.InsertCursor(fc_path, ["uuid", "name", "SHAPE@XY"]) as cursor:
+        for uuid, name, coordinates in stations_list:
+            cursor.insertRow([uuid, name, coordinates])
+    
+    # find nearest station
+    arcpy.analysis.Near(
+        in_features=selected_place,
+        near_features="measuring_stations",
+        location="LOCATION",
+        angle="NO_ANGLE",
+        method="PLANAR",
+        field_names="NEAR_FID NEAR_FID;NEAR_DIST NEAR_DIST;NEAR_X NEAR_X;NEAR_Y NEAR_Y",
+        distance_unit="Meters"
+    )
+
+    # get details of the nearest station
+    with arcpy.da.SearchCursor(
+        selected_place,
+        ["NEAR_DIST", "NEAR_FID"]
+    ) as cur:
+
+        for row in cur:
+            distance = row[0]
+            near_fid = row[1]
+
+    # get the actual ObjectID field name of measuring_stations
+    oid_field = arcpy.Describe("measuring_stations").OIDFieldName
+
+    # find the station with the corresponding ObjectID
+    where = f"{arcpy.AddFieldDelimiters('measuring_stations', oid_field)} = {near_fid}"
+
+    with arcpy.da.SearchCursor(
+        "measuring_stations",
+        [oid_field, "uuid", "name"],
+        where
+    ) as cur:
+
+        for row in cur:
+            uuid = row[1]
+            station_name = row[2]
+ 
+    # output
+    arcpy.AddMessage(f"Station name: {station_name}")
+    station_id = uuid
+    return distance, station_id
+
+
 
 # check if the API responds
 response = requests.get(f"https://pegelonline.wsv.de/webservices/rest-api/v2/stations.json")
@@ -117,38 +219,75 @@ json_data = response.json() if response and response.status_code == 200 else Non
 if json_data:
     # check if threre is data available for the nearest river
     response = requests.get(f"https://pegelonline.wsv.de/webservices/rest-api/v2/stations.json?waters={river}")
-    json_data = response.json() if response and response.status_code == 200 else None
-    if json_data:
-        arcpy.AddMessage(f"\nAPI data available for river {river}")
-        # TODO find nearest station for the river in question
-        # store its uuid in variable "station_id"
+    json_river_data = response.json() if response and response.status_code == 200 else None
+    if not json_river_data:
+        water_level_msg=f"No water levels available for the river {nearestName} via pegelonline API. The following water level data was measured at the nearest measuring station of any river: "
+        # find the nearest measuring station of any river and store its uuid in variable "station_id"
+        distance, station_id = findNearestStation(json_data)
     else:
-        arcpy.AddMessage(f"\nNo water levels available for {river} via this API.")
-        # TODO find nearest measuring station of any river
-        # store its uuid in variable "station_id"
-        # print(f"The nearest measuring station is {station}, measuring the water levels of {its river}.")
-
-    station_id = "b475386c-30cc-453a-b3b7-1d17ace13595" # static id for now
+        # find the nearest station for the river in question store its uuid in variable "station_id"
+        distance, station_id = findNearestStation(json_river_data)
 
     # check if measurements are available for this station
     response = requests.get(f"https://pegelonline.wsv.de/webservices/rest-api/v2/stations/{station_id}.json?includeTimeseries=true&includeCurrentMeasurement=true")
     json_data = response.json() if response and response.status_code == 200 else None
     if json_data:
-        # print water level info
-        arcpy.AddMessage(f"""Water level of the river {json_data['water']['longname']} 
+        # build water level info text
+        water_level_msg = water_level_msg +(f"""Water levels for the river {json_data['water']['longname']}  
             in {json_data['longname']} 
-            at {json_data['timeseries'][0]['currentMeasurement']['timestamp']}: 
-            {json_data['timeseries'][0]['currentMeasurement']['value']}{json_data['timeseries'][0]['unit']},
-            which is {json_data['timeseries'][0]['currentMeasurement']['stateMnwMhw']} for this river.""")
-            # TODO add average
-            # TODO add time series image of the past 15 days water levels
-            # TODO add forecast
-    else: arcpy.AddMessage("No water level data available for this station right now.")
+            at {json_data['timeseries'][0]['currentMeasurement']['timestamp']}:
+            {json_data['timeseries'][0]['currentMeasurement']['value']}{json_data['timeseries'][0]['unit']}.
+            Distance to measuring station: {distance}m""")
+        if(('stateMnwMhw' in json_data['timeseries'][0]['currentMeasurement']) and (json_data['timeseries'][0]['currentMeasurement']['stateMnwMhw'] != "unknown")):
+            water_level_msg = water_level_msg + (f"The current water level is {json_data['timeseries'][0]['currentMeasurement']['stateMnwMhw']} for this river.")
+        # print water level info
+        arcpy.AddMessage(water_level_msg)
 
-else: arcpy.AddMessage("No water levels API response.")
+        # get time series image of the past 30 days water levels
+        response = requests.get(f"https://pegelonline.wsv.de/webservices/rest-api/v2/stations/{station_id}/W/measurements.png?start=P30D&width=900&height=400")
+        if response.status_code == 200:
+            # create PDF
+            pdf_path = arcpy.GetParameterAsText(1)
+            pdf = SimpleDocTemplate(pdf_path, pagesize=letter)
+            styles = getSampleStyleSheet()
+            content = []
+
+            # heading:
+            content.append(Paragraph("Flood Risk Analysis Report", styles["Title"]))
+            content.append(Spacer(1, 12))
+
+            # text
+            body = f"""
+            {risk_text} <br/>
+            Nearest river: {nearestName} <br/>
+            Distance to the nearest river: {distance}m <br/>
+            Width of the nearest river: {nearestWidth}m <br/>
+            {water_level_msg} <br/>
+            Water level history for the past 30 days:
+            """
+            content.append(Paragraph(body, styles["Normal"]))
+            content.append(Spacer(1, 12))
+
+            # chart image as a reportlab flowable
+            chart = Image(BytesIO(response.content), width=500, height=300)
+            chart.hAlign = "CENTER"
+            content.append(chart)
+
+            pdf.build(content)
+
+            arcpy.SetParameterAsText(1, pdf_path)
+        else:
+            arcpy.AddMessage("No water level history available for this station right now.")
+
+        # TODO add forecast
+
+    else: water_level_msg = water_level_msg + ("No water level data available for this station right now.")
+
+else: water_level_msg = "No water levels available; the API does not respond."
 
 
-### weatherforecast
+
+# ---------- Weather forecast ------------------
 
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
--- final_project/README.md
+++ final_project/README.md
@@ -7,37 +7,37 @@
 
 - clone or download this repository (insert more detailed explanation)
 - import the atbx file into an ArcGIS project (insert more detailed explanation)
-- Download the zip file from ```https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/oberflaechengewaesser/``` gsk3e/ and import the shapefiles ```gsk3e_gewkz_line_breite.shp``` and/or ```gsk3e_gewkz_point_stat.shp``` into your ArcGIS project
+- Download the zip file from ```https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/oberflaechengewaesser/gsk3e/``` and import the shapefiles ```gsk3e_gewkz_line_breite.shp``` and/or ```gsk3e_gewkz_point_stat.shp``` into your ArcGIS project
 - Create a point layer with your point
 - to be continued
 
-https://pegelonline.wsv.de/webservice/guideRestapi
+API used: https://pegelonline.wsv.de/webservice/guideRestapi
 
 ### Data
-download the following data and load it into your ArcGIS project
+Download the following data and load it into your ArcGIS project
 
 - https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/hochwasser/hwrm/HQhaeufig-Ueberschwemmungsgrenzen_EPSG25832_Shape.zip
 
-  load the file ueberflutungsgrenzen_hohe_wahrscheinlichkeit into your ArcGIS project
+  load the file ```ueberflutungsgrenzen_hohe_wahrscheinlichkeit``` into your ArcGIS project
 
 - https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/hochwasser/hwrm/HQ100-Ueberschwemmungsgrenzen_EPSG25832_Shape.zip
 
-  load the file ueberflutungsgrenzen_mittlere_wahrscheinlichkeit into your ArcGIS project
+  load the file ```ueberflutungsgrenzen_mittlere_wahrscheinlichkeit``` into your ArcGIS project
 
 - https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/hochwasser/hwrm/HQextrem-Ueberschwemmungsgrenzen_EPSG25832_Shape.zip
 
-  load the file ueberflutungsgrenzen_niedrige Wahrscheinlichkeit into your ArcGIS project
+  load the file ```ueberflutungsgrenzen_niedrige Wahrscheinlichkeit``` into your ArcGIS project
 
 - https://www.opengeodata.nrw.de/produkte/umwelt_klima/wasser/oberflaechengewaesser/gsk3e/gsk3e_EPSG25832_Shape.zip
 
-  load the file gsk3e_gewkz_line_breite into your ArcGIS project
+  load the file ```gsk3e_gewkz_line_breite``` into your ArcGIS project
 
 ### Installation
-We use "Openmeteo" for our rain forecast. Therefore, you have to install two packages.
+We use "Openmeteo" for our rain forecast and reportlab for creating the PDF report. Therefore, you have to install three packages.
 
 ! The following instructions only work for Windows !
 
-- open the "Python Command Promt" (for ArcGIS Pro)
+- open the "Python Command Prompt" (for ArcGIS Pro)
 - clone your standard environment
 ```
 conda create -n your_env --clone arcgispro-py3
@@ -54,6 +54,7 @@ activate your_env
 ```
 pip install openmeteo-requests
 pip install requests-cache retry-requests numpy pandas
+conda install reportlab
 ```
 - open your ArcGIS Project
 - go to "Project" -> "Package Manager"
